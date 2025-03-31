a79603b2d85b8a1590bc780ba4554ea7c1314a66
Aleksandra500 <aleksandracomagic7@gmail.com>
2025-03-31 09:57:22 +0200

    finish

--- frontend/src/admin/AdminPage.jsx
+++ frontend/src/admin/AdminPage.jsx
@@ -3,7 +3,7 @@ import AdminNavComponent from './adminComponents/AdminNavComponent'
 import AllProdustPage from '../pages/AllProduct'
 function AdminPage({showAll}) {
   return (
-    <div>
+    <div >
       <AdminNavComponent />
       <div>
        {showAll && <AllProdustPage/>}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,18 +17,16 @@ import { Provider } from 'react-redux';
 import store from './store/store.js';
 
 //clerk
-import { ClerkProvider } from '@clerk/clerk-react';
+
 import AddProductsPage from './pages/AddProductsPage.jsx';
 import AllProduct from './pages/AllProduct.jsx';
 import AuthorizationPage from './pages/AuthorizationPage.jsx';
 import AdminPage from './admin/AdminPage.jsx';
 import AdminProtect from './admin/AdminProtect.jsx';
 // Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-if (!PUBLISHABLE_KEY) {
-    throw new Error('Missing Publishable Key');
-}
+
+
 
 const router = createBrowserRouter([
     {
@@ -79,9 +77,9 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+           
                 <RouterProvider router={router} />
-            </ClerkProvider>
+            
         </Provider>
     </React.StrictMode>
 );
